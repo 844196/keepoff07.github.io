40422580231e3b89293f8fbe6209b1d0bdc5617a
keepoff07 <keepoffandcurfew@yahoo.co.jp>
2015-11-21 09:19:55 +0900

    GistList : Gistからリストを更新,タグ機能の追加

--- gistlistdata/gist.js
+++ gistlistdata/gist.js
@@ -1,145 +1,219 @@
+var gists = new GistDataList();
+var tags = new TagList();
+var parm = new Query;
+
 $(function() {
 	//load
-	$.getJSON("gistlistdata/gist.json" , function(data) {
-		var cateBukkit = $('#category_bukkit');
-		var htmlBukkit = '';
-		for(var i in data.Bukkit) {
-			var id = data.Bukkit[i].id;
-			var title = data.Bukkit[i].title;
-			var desc = data.Bukkit[i].description;
-			htmlBukkit += '<div class="panel panel-default" id="'+id+'"><div class="panel-heading"><a class="title" href="?category=bukkit#'+id+'" style="font-size:14px">'+title+'</a></div><div class="panel-body">'+desc+'<div class="display"><hr><button type="button" class="btn btn-default btn-sm" id="code_view" style="display:inline">表示</button><button type="button" class="btn btn-default btn-sm" id="code_hide" style="display:none">非表示</button><br><div id="gistCode"></div></div></div><div class="panel-footer"><a href="https://gist.github.com/keepoff07/'+id+'"><div class="display">https://gist.github.com/keepoff07/'+id+'</div><div class="display-sm">https://gist...'+id+'</div></a></div></div>';
-		}
-		cateBukkit.html(htmlBukkit);
-		
-		var cateMC = $('#category_minecraft');
-		var htmlMC = '';
-		for(var i in data.Minecraft) {
-			var id = data.Minecraft[i].id;
-			var title = data.Minecraft[i].title;
-			var desc = data.Minecraft[i].description;
-			htmlMC += '<div class="panel panel-default" id="'+id+'"><div class="panel-heading"><a class="title" href="?category=minecraft#'+id+'" style="font-size:14px">'+title+'</a></div><div class="panel-body">'+desc+'<div class="display"><hr><button type="button" class="btn btn-default btn-sm" id="code_view" style="display:inline">表示</button><button type="button" class="btn btn-default btn-sm" id="code_hide" style="display:none">非表示</button><br><div id="gistCode"></div></div></div><div class="panel-footer"><a href="https://gist.github.com/keepoff07/'+id+'"><div class="display">https://gist.github.com/keepoff07/'+id+'</div><div class="display-sm">https://gist...'+id+'</div></a></div></div>';
-		}
-		cateMC.html(htmlMC);
-		
-		var cateOther = $('#category_other');
-		var htmlOther = '';
-		for(var i in data.Other) {
-			var id = data.Other[i].id;
-			var title = data.Other[i].title;
-			var desc = data.Other[i].description;
-			htmlOther += '<div class="panel panel-default" id="'+id+'"><div class="panel-heading"><a class="title" href="?category=other#'+id+'" style="font-size:14px">'+title+'</a></div><div class="panel-body">'+desc+'<div class="display"><hr><button type="button" class="btn btn-default btn-sm" id="code_view" style="display:inline">表示</button><button type="button" class="btn btn-default btn-sm" id="code_hide" style="display:none">非表示</button><br><div id="gistCode"></div></div></div><div class="panel-footer"><a href="https://gist.github.com/keepoff07/'+id+'"><div class="display">https://gist.github.com/keepoff07/'+id+'</div><div class="display-sm">https://gist...'+id+'</div></a></div></div>';
-		}
-		cateOther.html(htmlOther);
-		
-		setEvent();
+	// set data
+	$.getJSON("https://api.github.com/gists/501c035f20ff4213d041" , function(data) {
+		console.log("load");
+		$.getJSON(data.files.json.raw_url , function(subdata) {
+			for(var i in subdata) {
+				var gist = new GistData(subdata[i]);
+				tags.add(gist.tags);
+				gists.add(gist);
+			}
+			
+			// write
+			$('#tags').html(tags.ui);
+			var text = "";
+			for(var i = 0; i < gists.size(); i++) {
+				text += gists.get(i).html;
+			}
+			$('#content').html(text);
+			
+			// set event
+			setEvent();
+		});
 	});
 });
-
 function setEvent() {
-	$(function() {
-		$('body #code_hide').click(function(){
-			var button = $(this);
-			var parent = button.parents('.panel');
-			button.css('display', 'none');
-			parent.find('#code_view').css('display', 'inline');
-			parent.find('#gistCode').css('display', 'none');
-		});
-		$('body #code_view').click(function(){
-			var button = $(this);
-			var parent = button.parents('.panel');
-			var gistID = parent.attr('id');
-			var code = parent.find('#gistCode');
-			if(code.css('display') == 'block') {
-				var gistFrame = document.createElement("iframe");
-				gistFrame.setAttribute("width", "100%");
-				gistFrame.setAttribute("height", "210px");
-				gistFrame.setAttribute("scrolling", "auto");
-				gistFrame.id = "gistFrame";
-				
-				code.css('display', 'inline');
-				code.append(gistFrame);
-				
-				var gistFrameHTML = '<html><head></head><body><sc'+'ript type="text/javascript" src="https://gist.github.com/keepoff07/'+gistID+'.js"></sc'+'ript></body>';
-				var gistFrameDoc = gistFrame.document;
-				
-				if (gistFrame.contentDocument) {
-					gistFrameDoc = gistFrame.contentDocument;
-				} else if (gistFrame.contentWindow) {
-					gistFrameDoc = gistFrame.contentWindow.document;
-				}
-				gistFrameDoc.open();
-				gistFrameDoc.write(gistFrameHTML);
-				gistFrameDoc.close();
-			} else {
-				code.css('display', 'inline');
+	$('body #code_hide').click(function(){
+		var button = $(this);
+		var parent = button.parents('.panel');
+		button.css('display', 'none');
+		parent.find('#code_view').css('display', 'inline');
+		parent.find('#gistCode').css('display', 'none');
+	});
+	$('body #code_view').click(function(){
+		var button = $(this);
+		var parent = button.parents('.panel');
+		var gistID = parent.attr('id');
+		var code = parent.find('#gistCode');
+		if(code.css('display') == 'block') {
+			var gistFrame = document.createElement("iframe");
+			gistFrame.setAttribute("width", "100%");
+			gistFrame.setAttribute("height", "210px");
+			gistFrame.setAttribute("scrolling", "auto");
+			gistFrame.id = "gistFrame";
+			
+			code.css('display', 'inline');
+			code.append(gistFrame);
+			
+			var gistFrameHTML = '<html><head></head><body><sc'+'ript type="text/javascript" src="https://gist.github.com/keepoff07/'+gistID+'.js"></sc'+'ript></body>';
+			var gistFrameDoc = gistFrame.document;
+			
+			if (gistFrame.contentDocument) {
+				gistFrameDoc = gistFrame.contentDocument;
+			} else if (gistFrame.contentWindow) {
+				gistFrameDoc = gistFrame.contentWindow.document;
 			}
-			button.css('display', 'none');
-			parent.find('#code_hide').css('display', 'inline');
-		});
-		$('body a').click(function(){
-			var link = $(this);
-			if(link.attr('href').indexOf('category') != -1) {
-				var parent = $(link).parents('.panel');
-				var pos = $(parent).offset().top;
-				$("html,body").animate({scrollTop : pos}, 500);
-				return false;
-			} else {
-				return true;
-			}
-		});
-		
-		var cateBukkit = $('#category_bukkit');
-		var cateMC = $('#category_minecraft');
-		var cateOther = $('#category_other');
-		var setCateBukkit = $('body #set_category_bukkit');
-		var setCateMC = $('body #set_category_minecraft');
-		var setCateOther = $('body #set_category_other');
-		var search = location.search;
-		setCateBukkit.click(function(){
-			viewBukkit();
-		});
-		setCateMC.click(function(){
-			viewMC();
-		});
-		setCateOther.click(function(){
-			viewOther();
-		});
-		if(search.indexOf('category=minecraft') != -1) {
-			viewMC();
-		} else if(search.indexOf('category=other') != -1) {
-			viewOther();
+			gistFrameDoc.open();
+			gistFrameDoc.write(gistFrameHTML);
+			gistFrameDoc.close();
 		} else {
-			viewBukkit();
+			code.css('display', 'inline');
 		}
-		function viewBukkit() {
-			cateBukkit.css('display', 'inline');
-			cateMC.css('display', 'none');
-			cateOther.css('display', 'none');
-			setCateBukkit.attr('class', 'btn btn-default active');
-			setCateMC.attr('class', 'btn btn-default');
-			setCateOther.attr('class', 'btn btn-default');
+		button.css('display', 'none');
+		parent.find('#code_hide').css('display', 'inline');
+	});
+	$('body a').click(function(){
+		var link = $(this);
+		var hre = link.attr('href');
+		if(hre.substring(0, 5) == '?tag=') {
+			var tag = hre.substring(5, hre.length);
+			viewTag(tag);
+			return false;
 		}
-		function viewMC() {
-			cateBukkit.css('display', 'none');
-			cateMC.css('display', 'inline');
-			cateOther.css('display', 'none');
-			setCateBukkit.attr('class', 'btn btn-default');
-			setCateMC.attr('class', 'btn btn-default active');
-			setCateOther.attr('class', 'btn btn-default');
+		else if(hre.substring(0, 6) == '?clear') {
+			viewTag(null);
+			return false;
 		}
-		function viewOther() {
-			cateBukkit.css('display', 'none');
-			cateMC.css('display', 'none');
-			cateOther.css('display', 'inline');
-			setCateBukkit.attr('class', 'btn btn-default');
-			setCateMC.attr('class', 'btn btn-default');
-			setCateOther.attr('class', 'btn btn-default active');
-		}
-		var hash = location.hash;
-		if(hash.indexOf('#') != -1) {
-			var gist = $(hash);
-			var pos = $(gist).offset().top;
+		else if(hre.substring(0, 1) == '#') {
+			var parent = $(link).parents('.panel');
+			var pos = $(parent).offset().top;
 			$("html,body").animate({scrollTop : pos}, 500);
+			return false;
+		} else {
+			return true;
 		}
 	});
+
+	// tagパラメーターがある時
+	viewTag(parm.get("tag"));
+	
+	var hash = location.hash;
+	if(hash.indexOf('#') != -1) {
+		var gist = $(hash);
+		var pos = $(gist).offset().top;
+		$("html,body").animate({scrollTop : pos}, 500);
+	}
+}
+function viewTag(tag) {
+	$.each($('body').find('.panel'), function(index){
+		var div = $(this);
+		if(tag != null) {
+			var id = div.attr('id');
+			var gist = gists.search(id);
+			if(gist != null) {
+				if(gist.hasTag(tag)) {
+					div.css('display', 'block');
+				} else {
+					div.css('display', 'none');
+				}
+			}
+		} else {
+			div.css('display', 'block');
+		}
+	});
+}
+
+function GistData(data) {
+	this.id = data.id;
+	this.title = data.title;
+	this.depr = data.deprecated;
+	var dep = "";
+	if(this.depr) dep = "<b>@deprecated</b><br>";
+	this.desc = data.description;
+	this.tags = data.tags;
+	this.html = '<div class="panel panel-default" id="'+this.id+'"><div class="panel-heading"><a class="title" href="#'+this.id+'" style="font-size:14px">'+this.title+'</a></div><div class="panel-body">'+dep+this.desc+'<div class="display"><hr><button type="button" class="btn btn-default btn-sm" id="code_view" style="display:inline">表示</button><button type="button" class="btn btn-default btn-sm" id="code_hide" style="display:none">非表示</button><br><div id="gistCode"></div></div></div><div class="panel-footer"><a href="https://gist.github.com/keepoff07/'+this.id+'"><div class="display">https://gist.github.com/keepoff07/'+this.id+'</div><div class="display-sm">https://gist...'+this.id+'</div></a></div></div>';
+	this.hasTag = function(tag) {
+		for(var i in this.tags) {
+			if(this.tags[i] == tag)
+				return true;
+		}
+		return false;
+	}
+}
+function GistDataList() {
+	var gistlist = new Array();
+	this.add = function(gist) {
+		gistlist.push(gist);
+	}
+	this.size = function() {
+		return gistlist.length;
+	}
+	this.get = function(index) {
+		return gistlist[index];
+	}
+	this.search = function(id) {
+		for(var i in gistlist) {
+			if(gistlist[i].id == id)
+				return gistlist[i];
+		}
+		return null;
+	}
+}
+function TagList() {
+	var taglist = new Array();
+	this.add = function(tags) {
+		for(var i in tags) {
+			var tag = tags[i];
+			var enable = true;
+			for(var j in taglist) {
+				if(taglist[j] == tag) {
+					enable = false;
+					break;
+				}
+			}
+			if(enable) {
+				taglist.push(tag);
+			}
+		}
+	}
+	this.size = function() {
+		return taglist.length;
+	}
+	this.get = function(index) {
+		return taglist[index];
+	}
+	this.ui = function() {
+		var li = '<li role="presentation"><a href="?clear">ALL</a></li>';
+		for(var i in taglist) {
+			var tag = taglist[i];
+			li += '<li role="presentation"><a href="?tag='+tag+'">'+tag+'</a></li>'
+		}
+		return li;
+	}
+}
+function Query() {
+	this.has = false;
+	var keys = new Array();
+	var values = new Array();
+	
+	this.search = location.search;
+	var argu = this.search.split("?");
+	if(argu.length > 1) {
+		var args = argu[1].split("&");
+		for(var i = 0; i < args.length; i++ ) {
+			this.has = true;
+			var par = args[i].split("=");
+			keys.push(par[0]);
+			if(par.length > 1) {
+				values.push(par[1]);
+			} else {
+				values.push(null);
+			}
+		}
+	}
+	this.get = function(key) {
+		if(this.has) {
+			for(var i in keys) {
+				if(keys[i] == key) {
+					return values[i];
+				}
+			}
+		}
+		return null;
+	}
 }
